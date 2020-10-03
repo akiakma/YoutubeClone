9b0c7ded8f307b062b9549f1c6028aa0fec26881
akiakma <akiakmapro@gmail.com>
2020-10-03 21:47:31 +0900

    Updates

---
+++ precache-manifest.d245b83fc61578f1253344357c2b881e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cff6b34f47e7b618bf3ae718deaf3074",
+    "url": "/YoutubeClone/index.html"
+  },
+  {
+    "revision": "b38e9942513e441adefb",
+    "url": "/YoutubeClone/static/css/main.d4e563b7.chunk.css"
+  },
+  {
+    "revision": "b593e8db8bfccd941cc1",
+    "url": "/YoutubeClone/static/js/2.1424096d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/YoutubeClone/static/js/2.1424096d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b38e9942513e441adefb",
+    "url": "/YoutubeClone/static/js/main.0ed91bb8.chunk.js"
+  },
+  {
+    "revision": "bde6ecbe955bb4eb7ea1",
+    "url": "/YoutubeClone/static/js/runtime-main.72951500.js"
+  }
+]);
